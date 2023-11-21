5fcb76eaa576b5579523185c7f2635e32902bb41
Crobat4 <104547700+Crobat4@users.noreply.github.com>
2023-11-21 03:06:29 -0300

    config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,25 @@
 import { defineConfig } from 'vite';
 import preact from '@preact/preset-vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [preact(), tsconfigPaths()],
+    build: {
+        // generate .vite/manifest.json in outDir
+        manifest: true,
+        rollupOptions: {
+            // overwrite default .html entry
+            input: ['src/index.tsx'],
+        },
+    },
+    resolve: {
+        alias: {
+            components: path.resolve(__dirname, './src/components'),
+            assets: path.resolve(__dirname, './src/assets'),
+            modules: path.resolve(__dirname, './src/modules'),
+            styles: path.resolve(__dirname, './src/styles'),
+        },
+    },
 });
