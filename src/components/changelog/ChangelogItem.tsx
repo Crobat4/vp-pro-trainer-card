152a3d24294fb29f2185670a5cb8640d450280a5
Crobat4 <104547700+Crobat4@users.noreply.github.com>
2023-12-12 23:39:17 -0300

    Update ChangelogItem.tsx

--- src/components/changelog/ChangelogItem.tsx
+++ src/components/changelog/ChangelogItem.tsx
@@ -11,7 +11,7 @@ type Props = {
 function ChangelogItem({ key, title, description, variant, active = false }: Props) {
     return (
         <tr className={active ? 'table-active' : ''}>
-            <td style={{width: '1%'}}>
+            <td style={{ width: '1%', verticalAlign: 'middle' }}>
                 <span className={`badge w-100 text-uppercase text-bg-${variant}`}>{title}</span>
             </td>
             <td>
