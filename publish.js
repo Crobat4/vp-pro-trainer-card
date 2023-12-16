9981c3ea5344a55f076f8905190550853b9a3aa3
Crobat4 <104547700+Crobat4@users.noreply.github.com>
2023-12-16 03:40:12 -0300

    Publish script

--- publish.js
+++ publish.js
@@ -3,13 +3,27 @@
 const ghpages = require('gh-pages');
 const { version } = require('./package.json');
 
+const fs = require('fs');
+
+// destination will be created or overwritten by default.
+
+const files = ['README.md', 'package.json', '.gitignore', '.github'];
+files.map((file) => {
+    fs.cp(file, `dist/${file}`, {recursive: true}, (err) => {
+        if (err) {
+            throw err;
+        }
+        console.info(`${file} was copied to destination`);
+    });
+});
+
+/*
 console.info('Starting publish..');
 
-ghpages.publish('docs', {
-    src: ['**/*', '../README.md'],
-    branch: 'master-bak4',
-    dest: 'docs',
+ghpages.publish('dist', {
+    branch: 'master-bak15',
     message: `[v${version}] Live website`,
+    dotfiles: true,
 }, (err) => {
     if (err) {
         console.error('Something went wrong publishing...\n', err);
@@ -17,3 +31,4 @@ ghpages.publish('docs', {
         console.info('Successfully published repo to GitHub pages!');
     }
 });
+*/
